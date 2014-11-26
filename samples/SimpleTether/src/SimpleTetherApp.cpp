4dfa3a391b6df2f8bb1df068cb9a3c785d40d7d8
hezhao <zhao.he@wk.com>
2014-11-25 16:48:37 -0800

    Removed Cinder dependency in SimpleTetherApp

--- samples/SimpleTether/src/SimpleTetherApp.cpp
+++ samples/SimpleTether/src/SimpleTetherApp.cpp
@@ -1,32 +1,29 @@
-
-#include "cinder/app/AppNative.h"
-#include "cinder/Utilities.h"
-#include "cinder/gl/gl.h"
-#include "cinder/gl/Texture.h"
-
+#include <boost/filesystem.hpp>
+//#include "cinder/app/AppNative.h"
 #include "Cinder-EDSDK.h"
 
-using namespace ci;
-using namespace ci::app;
-using namespace Cinder::EDSDK;
-using namespace std;
 
-void drawTextureInRect(gl::Texture texture, Rectf rect, bool fit = true) {
-    if (fit) {
-        Rectf textureRect = Rectf(Vec2i::zero(), texture.getSize());
-        Rectf r = textureRect.getCenteredFit(rect, true);
-        gl::draw(texture, r);
-    } else {
-        Area a =  Area(rect.getCenteredFit(texture.getBounds(), true));
-        gl::draw(texture, a, rect);
-    }
-}
+using namespace EDSDK;
+//using namespace ci;
+//using namespace ci::app;
 
-class SimpleTetherApp : public AppNative {
+
+//void drawTextureInRect(gl::Texture texture, Rectf rect, bool fit = true) {
+//    if (fit) {
+//        Rectf textureRect = Rectf(Vec2i::zero(), texture.getSize());
+//        Rectf r = textureRect.getCenteredFit(rect, true);
+//        gl::draw(texture, r);
+//    } else {
+//        Area a =  Area(rect.getCenteredFit(texture.getBounds(), true));
+//        gl::draw(texture, a, rect);
+//    }
+//}
+
+class SimpleTetherApp {
 public:
-    void prepareSettings(Settings* settings);
+//    void prepareSettings(Settings* settings);
     void setup();
-    void keyDown(KeyEvent event);
+    void keyDown();
     void draw();
 
     void browserDidAddCamera(CameraRef camera);
@@ -38,49 +35,44 @@ public:
 
 private:
     CameraRef mCamera;
-    gl::Texture mPhotoTexture;
+//    gl::Texture mPhotoTexture;
 };
 
-void SimpleTetherApp::prepareSettings(Settings* settings) {
-    settings->enableHighDensityDisplay();
-    settings->setWindowSize(640, 480);
-}
+//void SimpleTetherApp::prepareSettings(Settings* settings) {
+//    settings->enableHighDensityDisplay();
+//    settings->setWindowSize(640, 480);
+//}
 
-void SimpleTetherApp::setup() {
+void SimpleTetherApp::setup()
+{
     CameraBrowser::instance()->connectAddedHandler(&SimpleTetherApp::browserDidAddCamera, this);
     CameraBrowser::instance()->connectRemovedHandler(&SimpleTetherApp::browserDidRemoveCamera, this);
     CameraBrowser::instance()->connectEnumeratedHandler(&SimpleTetherApp::browserDidEnumerateCameras, this);
     CameraBrowser::instance()->start();
 }
 
-void SimpleTetherApp::keyDown(KeyEvent event) {
-    switch (event.getCode()) {
-        case app::KeyEvent::KEY_SPACE:
+void SimpleTetherApp::keyDown()
+{
+    switch (getchar()) {
+        case '\n':
             if (mCamera != NULL && mCamera->hasOpenSession()) {
                 mCamera->requestTakePicture();
             }
-            break;
-        case app::KeyEvent::KEY_ESCAPE:
-            quit();
             break;
         default:
             break;
     }
 }
 
-void SimpleTetherApp::draw() {
-	gl::clear(Color::black());
-
-    if (mPhotoTexture != NULL) {
-        gl::color(Color::white());
-        drawTextureInRect(mPhotoTexture, Rectf(Vec2i::zero(), getWindowSize()), false);
-    }
+void SimpleTetherApp::draw()
+{
+    
 }
 
 #pragma mark - CAMERA BROWSER
 
 void SimpleTetherApp::browserDidAddCamera(CameraRef camera) {
-    console() << "added a camera: " << camera->getName() << std::endl;
+    std::cout << "added a camera: " << camera->getName() << std::endl;
     if (mCamera != NULL) {
         return;
     }
@@ -88,57 +80,70 @@ void SimpleTetherApp::browserDidAddCamera(CameraRef camera) {
     mCamera = camera;
     mCamera->connectRemovedHandler(&SimpleTetherApp::didRemoveCamera, this);
     mCamera->connectFileAddedHandler(&SimpleTetherApp::didAddFile, this);
-    console() << "grabbing camera: " << camera->getName() << std::endl;
+    std::cout << "grabbing camera: " << camera->getName() << std::endl;
 
-    Cinder::EDSDK::Camera::Settings settings = Cinder::EDSDK::Camera::Settings();
+    EDSDK::Camera::Settings settings = EDSDK::Camera::Settings();
 //    settings.setPictureSaveLocation(kEdsSaveTo_Both);
 //    settings.setShouldKeepAlive(false);
     EdsError error = mCamera->requestOpenSession(settings);
     if (error == EDS_ERR_OK) {
-        console() << "session opened" << std::endl;
+        std::cout << "session opened" << std::endl;
     }
 }
 
 void SimpleTetherApp::browserDidRemoveCamera(CameraRef camera) {
     // NB - somewhat redundant as the camera will notify handler first
-    console() << "removed a camera: " << camera->getName() << std::endl;
+    std::cout << "removed a camera: " << camera->getName() << std::endl;
     if (camera != mCamera) {
         return;
     }
 
-    console() << "our camera was disconnected" << std::endl;
+    std::cout << "our camera was disconnected" << std::endl;
     mCamera = NULL;
 }
 
 void SimpleTetherApp::browserDidEnumerateCameras() {
-    console() << "enumerated cameras" << std::endl;
+    std::cout << "enumerated cameras" << std::endl;
 }
 
 #pragma mark - CAMERA
 
 void SimpleTetherApp::didRemoveCamera(CameraRef camera) {
-    console() << "removed a camera: " << camera->getName() << std::endl;
+    std::cout << "removed a camera: " << camera->getName() << std::endl;
     if (camera != mCamera) {
         return;
     }
 
-    console() << "our camera was disconnected" << std::endl;
+    std::cout << "our camera was disconnected" << std::endl;
     mCamera = NULL;
 }
 
-void SimpleTetherApp::didAddFile(CameraRef camera, CameraFileRef file) {
-//    fs::path destinationFolderPath = expandPath(fs::path("~/Desktop/Captures"));
-//    camera->requestDownloadFile(file, destinationFolderPath, [&](EdsError error, ci::fs::path outputFilePath) {
-//        if (error == EDS_ERR_OK) {
-//            console() << "image downloaded to '" << outputFilePath << "'" << std::endl;
-//        }
-//    });
-
-    camera->requestReadFile(file, [&](EdsError error, ci::Surface surface) {
-        if (error == EDS_ERR_OK) {
-            mPhotoTexture = gl::Texture(surface);
-        }
-    });
+int main()
+{
+    SimpleTetherApp *app = new SimpleTetherApp();
+    app->setup();
+    while (1)
+    {
+        app->keyDown();
+    }
+    
+    return 0;
 }
 
-CINDER_APP_NATIVE(SimpleTetherApp, RendererGl)
+void SimpleTetherApp::didAddFile(CameraRef camera, CameraFileRef file)
+{
+    boost::filesystem::path destinationFolderPath = boost::filesystem::path("/Users/zhao.he/Desktop/Captures");
+    camera->requestDownloadFile(file, destinationFolderPath, [&](EdsError error, boost::filesystem::path outputFilePath) {
+        if (error == EDS_ERR_OK) {
+            std::cout << "image downloaded to '" << outputFilePath << "'" << std::endl;
+        }
+    });
+
+//    camera->requestReadFile(file, [&](EdsError error, ci::Surface surface) {
+//        if (error == EDS_ERR_OK) {
+//            mPhotoTexture = gl::Texture(surface);
+//        }
+//    });
+}
+
+//CINDER_APP_NATIVE(SimpleTetherApp, RendererGl)
